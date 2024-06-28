2e5ed6caa9e5d086a916d33674a27bbc3b2db326
deioh <143625578+deioh@users.noreply.github.com>
2024-06-28 22:15:08 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import { RecipeList } from './RecipeList/RecipeList';
 import recipes from '../data/recipes.json';
+
 export const App = () => {
-  //  return <RecipeList />;
   return <RecipeList recipes={recipes} />;
 };
+
+// import { RecipeList } from './RecipeList/RecipeList';
+// import recipes from '../data/recipes.json';
+// export const App = () => {
+//   //  return <RecipeList />;
+//   return <RecipeList recipes={recipes} />;
+// };
--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,20 +1,24 @@
-//import React from 'react';
-
-import { RecipeListItem } from 'components/RecipeListItem/RecipeListItem';
-
 export const RecipeList = ({ recipes }) => {
   return (
     <ul>
-      {recipes.map(
-        ({ key, title }) => (
-          <RecipeListItem key={key} title={title} />
-        )
-
-        // <li>
-        //   <h3>{recipe.title}</h3>
-        //   <h4>{recipe.description}</h4>
-        // </li>
-      )}
+      {recipes.map(recipe => (
+        <li>
+          <h3>{recipe.title}</h3>
+          <img src={recipe.image} alt={recipe.title} width={300} height={300} />
+        </li>
+      ))}
     </ul>
   );
 };
+
+// import { RecipeListItem } from 'components/RecipeListItem/RecipeListItem';
+
+// export const RecipeList = ({ recipes }) => {
+//   return (
+//     <ul>
+//       {recipes.map(({ key, title }) => (
+//         <RecipeListItem key={key} title={title} />
+//       ))}
+//     </ul>
+//   );
+// };
